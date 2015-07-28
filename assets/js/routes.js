10a5b520a3c5a250455ca0326ca07e9e1d782856
Eric Liang <liang.eric24@gmail.com>
2015-07-28 00:08:31 -0700

    added nprogress bar, week 2-5 pages

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -11,4 +11,38 @@ app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
       url: '/1',
       templateUrl: './pages/week-1.html'
     })
+    .state('2', {
+      url: '/2',
+      templateUrl: './pages/week-2.html'
+    })
+    .state('3', {
+      url: '/3',
+      templateUrl: './pages/week-3.html'
+    })
+    .state('4', {
+      url: '/4',
+      templateUrl: './pages/week-4.html'
+    })
+    .state('5', {
+      url: '/5',
+      templateUrl: './pages/week-5.html'
+    })
 })
+
+
+app.run(['$rootScope', '$location', '$window', function ($rootScope, $location, $window) {
+  $rootScope.$on('$stateChangeStart', function () {
+    // start the loading bar and set it to a reasonable position
+    NProgress.start();
+    NProgress.set(0.3);
+  });
+
+  $rootScope.$on('$stateChangeSuccess', function () {
+    // scroll to top of page
+    $('body').scrollTop(0);
+
+    // end the progress bar
+    NProgress.done();
+  });
+
+}])
