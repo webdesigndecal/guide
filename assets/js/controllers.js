c05c4c603183fcc0282ac80d6cc477b348115f2c
Eric Liang <liang.eric24@gmail.com>
2015-07-28 18:25:12 -0700

    implemented copy to clipboard functionality

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -7,3 +7,18 @@ app.controller('NavController', ['$scope', '$location', function ($scope, $locat
 app.controller('HomeController', ['$scope', function ($scope) {
   this.lectures = allLectures;
 }])
+
+app.controller('ArticleController', ['$scope', function ($scope) {
+  $scope.copyText = function (event) {
+
+    // getting the <code> div
+    var target = angular.element(event.target)[0].nextElementSibling;
+    text = target.innerText;
+    var textField = document.createElement('textarea');
+    textField.innerText = text;
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand('copy');
+    textField.remove();
+  }
+}])
